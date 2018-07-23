103a04fd74c2d8da6add89f0a09c9cb42fa0cb14
loangelak <loangela@optonline.net>
2018-07-23 15:30:14 -0400

    Create sigma1.c

---
+++ sigma1.c
@@ -0,0 +1,30 @@
+//sums a range of numbers recursively
+#include <cs50.h>
+#include <stdio.h>
+
+int sigma(int m);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Positive integer: ");
+    }
+    while (n < 1);
+    int answer = sigma(n);
+    printf("%i\n", answer);
+}
+
+//Returns sm of 1 through m
+int sigma(int m)
+{
+    if (m <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return (m + sigma(m-1));
+    }
+}
