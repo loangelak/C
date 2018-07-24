74052bc92f7c342756f2a039886115f355003cbe
loangelak <loangela@optonline.net>
2018-07-24 14:19:39 -0400

    Update capitalizefirst.c

--- capitalizefirst.c
+++ capitalizefirst.c
@@ -1,4 +1,4 @@
-//capitalizes a string
+//capitalizes a copy of a string while checking for errors
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -7,17 +7,32 @@
 int main(void)
 {
     //get a string
-    string s = get_string("s: ");
-
-    //copy string's address
-    string t = s;
-
-    //capitalize first letter in string
+    char *s = get_string("s: ");
+    if(!s)
+    {
+        return 1;
+    }
+    //allocate memory for another string
+    char *t = malloc((strlen(s) + 1) * sizeof(char));
+    if(!t)
+    {
+        return 1;
+    }
+    //copy string into memory
+    for(int i = 0, n = strlen(s); i <= n; i++)
+    {
+        t[i] = s[i];
+    }
+    //capitalize first letter in copy
     if(strlen(t) > 0)
     {
         t[0] = toupper(t[0]);
     }
-    //pring string twice
+    //print strings
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    //free memory
+    free(t);
+    return 0;
 }
